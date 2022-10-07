90999a9019f4ab698321618729955ca713dc88a9
kacarervin <kacarervin@gmail.com>
2022-10-07 16:09:08 +0200

    Add style for expense list

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const ExpenseItem = () => {
-  return <div>ExpenseItem</div>;
+const ExpenseItem = ({ expense }) => {
+  const { id, charge, amount } = expense;
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expense">{charge}</span>
+        <span className="amount">${amount}</span>
+      </div>
+    </li>
+  );
 };
 
 export default ExpenseItem;
