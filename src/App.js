0f310c3c0e56b001c65dafd41d3010fe86f2fc4f
kacarervin <kacarervin@gmail.com>
2022-10-07 14:53:02 +0200

    Finish folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import Alert from "./components/Alert";
 
 function App() {
-  return <div className="App">Hello from app</div>;
+  return (
+    <>
+      <Alert />
+      <ExpenseForm />
+      <ExpenseList />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+
+const ExpenseList = () => {
+  return (
+    <>
+      <div>hello from expense list</div>
+      <ExpenseItem />
+    </>
+  );
+};
+
+export default ExpenseList;
